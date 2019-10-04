d7d1ee85217bf70ba4b29b21f5a6021143f7379f
rajesh3110 <55232937+rajesh3110@users.noreply.github.com>
2019-10-04 17:15:33 +0530

    4/10/19

---
+++ numino/src/demos/ThreadLifeExecution.java
@@ -0,0 +1,93 @@
+class thread implements Runnable 
+{
+	
+	 public void run() 
+	    { 
+	      
+	        try
+	        { 
+	            Thread.sleep(1500); 
+	        }  
+	        catch (InterruptedException e)  
+	        { 
+	            e.printStackTrace(); 
+	        } 
+	          
+	        System.out.println("State of  -"+ 
+	        		ThreadLifeExecution.thread1.getState()); 
+	        try
+	        { 
+	            Thread.sleep(200); 
+	        }  
+	        catch (InterruptedException e)  
+	        { 
+	            e.printStackTrace(); 
+	        }      
+	    } 
+		
+}
+
+
+
+public class ThreadLifeExecution implements Runnable {
+
+	
+	 public static Thread thread1; 
+	 public static ThreadLifeExecution obj; 
+	 
+	 
+	 
+	 
+	
+	
+	   
+	    public static void main(String[] args) 
+	    { 
+	        obj = new ThreadLifeExecution(); 
+	        thread1 = new Thread(obj); 
+	          
+	        
+	        System.out.println("State of 1 Thread created   " + thread1.getState()); 
+	        thread1.start(); 
+	          
+	       
+	        System.out.println("State of  1 Thread  " +  
+	            thread1.getState()); 
+	    } 
+	      
+	    public void run() 
+	    { 
+	        thread myThread = new thread(); 
+	        Thread thread2 = new Thread(myThread); 
+	          
+	        System.out.println("State of 2 Thread created "+ thread2.getState()); 
+	        thread2.start(); 
+	          
+	        System.out.println("State   2 Thread  " +  
+	            thread2.getState()); 
+	          
+	        try
+	        { 
+	            Thread.sleep(200); 
+	        }  
+	        catch (InterruptedException e)  
+	        { 
+	            e.printStackTrace(); 
+	        } 
+	        System.out.println("State of  2 Thread - "+  
+	            thread2.getState() ); 
+	          
+	          
+	        try 
+	        { 
+	            thread2.join(); 
+	        }  
+	        catch (InterruptedException e)  
+	        { 
+	            e.printStackTrace(); 
+	        } 
+	        System.out.println("State of  2 Thread - " +  
+	            thread2.getState()); 
+	    } 
+	      
+	} 
